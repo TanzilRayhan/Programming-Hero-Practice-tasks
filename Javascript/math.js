9fbb8d53a3eafc5416dc91629292eb3ab6e95676
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 02:03:23 +0600

    math.js

---
+++ Javascript/math.js
@@ -0,0 +1,16 @@
+/*
+
+1. Numbers
+2. Integers
+3. Float
+4. Mathematical Operations
+5. Percentage
+6. Average
+7. Comparison
+8. Basic Maths
+9. Prime
+10. Series
+11. Permutation
+12. Area ......
+
+*/
