c14285deb2ef466668c7c017373ed8c941363221
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 02:30:20 +0600

    Variable type

--- Javascript/variable.js
+++ Javascript/variable.js
@@ -1,9 +1,17 @@
 // Vary + Able = Variable
 
+//** Data type of variables **
+
+// Numeric
 var price = 1000;
 var age = 20;
 var temperature = 36;
+
+// String
 var name = "Rust";
 var device = "Laptop"
 var food = "Fried Rice"
 
+// Boolean
+var goal = true;
+var out = false;
