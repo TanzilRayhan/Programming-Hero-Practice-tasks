a881ea2caffea44177814cb8cbf5fb884914cfcd
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 02:45:04 +0600

    Variable name

---
+++ Javascript/variable-name.js
@@ -0,0 +1,25 @@
+// JavaScript Keyword, Variable name naming convention
+
+// 1. Variable name can't be the javascript reserved keywords
+var true = 10;
+var const = 1000;
+
+// 2. variable name should be in one words (No space)
+var Full Name = "Tom Hanks";
+
+// 3. variable name can't have quotation
+var "Price" = 50;
+
+// 4. variable name can't starts with a number but can contains number
+var 5star = 5;
+
+// 5. Can't use dash but can use underscore
+var user-name = "Rock";
+var user_name = "Rock";
+
+//Camel Case
+var userNameInput = "Toy"
+//Pascal case
+var UserNameInput = "Toy"
+//Snake Case
+var user_name_input = "Toy"
