cafdf4463b6fdba6d03c76378903da846e3f2628
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 03:14:05 +0600

    operation.js

---
+++ Javascript/operation.js
@@ -0,0 +1,20 @@
+var apple = 500;
+var mango = 250;
+
+// console.log(apple);
+// console.log(mango);
+// console.log(mango+apple);
+
+// addition
+var totalPrice = apple + mango;
+console.log(totalPrice);
+// subtraction
+var priceDifference = apple - mango;
+console.log(priceDifference);
+// multiplication
+var quantity = 20;
+var totalApple = apple * quantity;
+console.log(totalApple);
+//Division
+var singlePrice = apple/quantity;
+console.log(singlePrice);
