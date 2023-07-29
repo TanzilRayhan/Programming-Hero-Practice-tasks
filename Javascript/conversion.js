a70445d9da25990f5a81df905e306205a9638883
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 18:29:35 +0600

    conversion

---
+++ Javascript/conversion.js
@@ -0,0 +1,11 @@
+var price1 = '15';
+var price2 = '20';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+// convert string to number/float = parseInt/parseFloat
+var price1Int = parseInt(price1);
+var price2Int = parseInt(price2);
+var totalPriceInt = price1Int + price2Int;
+console.log(totalPriceInt);
+
