0f1b4149c000020717736f24fed9ca5d9dbfcba9
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 18:04:49 +0600

    shorthand

---
+++ Javascript/shorthand.js
@@ -0,0 +1,33 @@
+// variable declaration
+var ball = 10;
+// variable value update
+ball = 25;
+// console.log(ball);
+
+//set new value
+var price1 = 15;
+var price2 = 20;
+
+price1 = price1 + 20;
+console.log(price1);
+
+// Shorthand +=
+// += means adding value to the existing value
+price1 += 20;
+
+// Shorthand -=
+// += means deducting value from the existing value
+price2 -= 5;
+console.log(price2);
+
+// Shorthand *=
+price1 *= 2;
+
+// Shorthand /=
+price2 /= 5;
+
+var age = 20;
+// ++ -- add or deduct 1
+age++;
+age--;
+console.log(age);
