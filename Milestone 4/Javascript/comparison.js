d6b7c09037242bf5f7e1c2485cc528b500ab5854
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-30 03:15:12 +0600

    comparison

---
+++ Milestone 4/Javascript/comparison.js
@@ -0,0 +1,21 @@
+//less than
+console.log( 5<10);
+// greater than
+console.log( 5>10);
+//equal
+console.log( 5==10);
+console.log( 10==10);
+//not equal;
+console.log( 5!=10);
+//less than equal
+console.log( 5<=10);
+console.log( 5<=5);
+//greater than equal
+console.log( 5>=10);
+console.log( 15>=10);
+
+// Multiple condition
+// AND
+// num1 > num2 && num1 < num3
+// OR
+// num1 > num2 || num1 < num3
