b672fa5abfb5c0ea30d2558f8d6ff3cd5e58d709
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 18:23:33 +0600

    concatenation

---
+++ Javascript/contatenation.js
@@ -0,0 +1,10 @@
+var apple = 300;
+var mango = 250;
+var totalPrice = apple + mango;
+// console.log(totalPrice);
+
+//concatenation
+var firstName = "John"
+var lastName = "Right"
+var fullName = firstName + ' ' + lastName ;
+console.log(fullName);
