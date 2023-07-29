331d08e62cc046b56d70218262d73b9c3f89f5da
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-29 19:18:05 +0600

    types

---
+++ Javascript/types.js
@@ -0,0 +1,17 @@
+var price = 100;
+console.log(typeof price);
+var quantity = '100';
+console.log(typeof quantity);
+var isGoal = true;
+console.log(typeof isGoal);
+var phone;
+console.log(typeof phone);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log(sum);
+
+
