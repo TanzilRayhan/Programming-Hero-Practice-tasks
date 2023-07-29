2954ae17957f9da27455b3b37fb29dffdd40ffb9
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-30 02:57:17 +0600

    push pop

---
+++ Milestone 4/Javascript/push-pop.js
@@ -0,0 +1,14 @@
+var age = [5, 10, 15, 7];
+
+// push to add element at last
+age.push(20);
+console.log(age);
+
+// pop to remove an element from last
+var names = ['top', 'rock', 'pot'];
+names.pop();
+console.log(names);
+
+// popped element
+var element = names.pop();
+console.log(element);
