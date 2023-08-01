eb554ce77a49e0f959b74628a97c4ada906f2ffd
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-08-01 16:47:51 +0600

    multiple conditions

---
+++ Milestone 4/Javascript/multiple-conditions.js
@@ -0,0 +1,21 @@
+var car = 100;
+var bike = 500;
+
+//else
+if (car <=100 && bike>=300){
+    console.log("Capacity full");
+}
+else{
+    console.log("Capacity does not full")
+}
+
+// else if
+if (car<100 && bike<300){
+    console.log("Has Capacity");
+}
+else if(car=100){
+    console.log("Capacity full");
+}
+else{
+    console.log("Capacity does not full");
+}
