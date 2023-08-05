d69b9910bdea5962884f5ba0718f7357970c21b6
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-08-06 02:50:57 +0600

    object

---
+++ Milestone 4/Javascript/object.js
@@ -0,0 +1,13 @@
+//object declare
+var student = {
+    id: 100,
+    name: "Rox",
+    class: 10,
+    gpa: 5
+}
+
+console.log(student);
+console.log(student.gpa);
+//change value
+student.class = 11;
+console.log(student);
