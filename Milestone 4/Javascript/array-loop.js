81f7d3b7d634dff8f4015d2eefc291451f240936
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-08-05 18:28:15 +0600

    array-loop

---
+++ Milestone 4/Javascript/array-loop.js
@@ -0,0 +1,13 @@
+// var numbers = [5, 3, 9, 2, 7, 10, 1];
+
+// for(var i=0; i<numbers.length; i++){
+//     var num = numbers[i];
+//     console.log(num);
+// }
+
+var fruits = ['apple', 'mango', 'lichi', 'blueberry','dragon'];
+
+for(var i=0; i<fruits.length; i++){
+    var item = fruits[i];
+    console.log(item);
+}
