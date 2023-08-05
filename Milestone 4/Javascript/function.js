e399c2013b5a4f3026eb60869804487bb17d4b4a
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-08-06 01:33:14 +0600

    function

---
+++ Milestone 4/Javascript/function.js
@@ -0,0 +1,14 @@
+//function declaration
+function print(){
+    console.log("Hello World");
+    console.log("Welcome");
+    console.log("to this Universe");
+}
+//call the function
+print();
+
+//multiple parameter
+function add(num1, num2){
+    console.log("Hello World", num1, num2);
+}
+add(10,15);
